3634b1e88906ca66bf5022347cece87a55abd736
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-03-27 16:25:46 -0700

    Chain validation

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,21 +2,67 @@ const Blockchain = require('./blockchain');
 const Block = require('./block');
 
 describe('Blockchain', () => {
-  const blockchain = new Blockchain();
+  let blockchain;
+
+  beforeEach(() => {
+    blockchain = new Blockchain();
+  })
 
   it('contains a `chain` Array instance', () => {
     expect(blockchain.chain instanceof Array).toBe(true);
   });
 
   it('starts with the genesis block', () => {
-    expect(blockchain.chain[0].toEqual(Block.genesis()));
+    expect(blockchain.chain[0]).toEqual(Block.genesis());
   })
 
-  it('add a new block to the chain', () => {
+  it('adds a new block to the chain', () => {
     const newData = 'foo bar';
     blockchain.addBlock({ data: newData });
 
     expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
+  });
+
+  describe('isValidChain()', () => {
+    describe('when the chain does not start with the genesis block', () => {
+      it('returns false', () => {
+        blockchain.chain[0] = { data: 'fake-genesis'};
+
+        expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+      });
+    });
+
+    describe('when the chain starts with the genesis bllock and has multiple blocks', () => {
+      beforeEach(() => {
+        blockchain.addBlock({ data: 'panda'});
+        blockchain.addBlock({ data: 'koala'});
+        blockchain.addBlock({ data: 'bunny'});
+      });
+
+      describe('and a lastHash reference has changed', () => {
+        it('returns false', () => {
+          blockchain.chain[2].lashHash = 'broken-lastHash';
+
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+        });
+      });
+
+      describe('and the chain contains a block with an invalid field', () => {
+        it('returns false', () => {
+
+          blockchain.chain[2].data = 'some-bad-data';
+
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+        });
+      });
+
+      describe('and the chain does not contain any invalid blocks', () => {
+        it('returns true', () => {
+
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
+        });
+      });
+    });
   });
 });
 
